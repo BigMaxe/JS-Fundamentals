9cc9f3b8026f90823dfda40fb78ba50291a8a1cd
Yaoduye <yaoduyeogbuleka@gmail.com>
2025-07-09 20:52:19 +0100

    Add 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,4 +1,13 @@
-const arg = "89"; 
+// const arg = "89"; 
+// const num = parseInt(arg);
+
+// if (isNaN(num)) {
+//     console.log("Not a number");
+// } else {
+//     console.log("My number: " + num);
+// }
+
+const arg = process.argv[2];
 const num = parseInt(arg);
 
 if (isNaN(num)) {
