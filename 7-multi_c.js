20afb6f94a2d33f6c011884fa7b1772b964f7582
Yaoduye <yaoduyeogbuleka@gmail.com>
2025-07-09 21:16:55 +0100

    Add 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+// let x = parseInt(process.argv["7"]);
+let x = parseInt("7");
+
+if (isNaN(x)) {
+    console.log("Missing number of occurrences");
+} else {
+    for (let i = 0; i < x; i++) {
+        console.log("C is fun");
+    }
+}
